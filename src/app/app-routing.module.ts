47a031ccc829f939b0481432fc8a9b4083a50622
Jajhoey <john.hoey15@my.stjohns.edu>
2020-11-25 18:21:22 -0500

    Angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//This array constant can be used to reference all routing components
+//without the need to import each one seperately again in app.modules
+export const routingComponents = [AboutComponent, ProjectsComponent]
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class NavBarComponent implements OnInit {
+  onNavClick(){
+    
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
